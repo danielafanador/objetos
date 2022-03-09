1c3bfc0515d39fbdcff17bc0437e993ba0087c8e
Daniel Afanador <danielthemisfits@gmail.com>
2022-03-08 22:24:08 -0300

    La versión anterior no tenía datos

---
+++ objetos.js
@@ -0,0 +1,12 @@
+var miEquipo = { 
+	nombre: 'Millonarios',
+	camiseta: 'Adidas',
+	títulos: 15,
+	color: 'Azul',
+	ciudad: 'Bogotá',
+
+	detallesDelEquipo: function () {
+		console.log (`Mi equipo es ${this.nombre} de ${this.ciudad}`)
+	}
+
+ }
